c059f9471d1f5778111a32c2ea9a210866a5e158
villa588 <ryan.r.villasenor@gmail.com>
2025-11-28 12:30:28 -0800

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -6,3 +6,12 @@ print("New Pyhon File")
 url="https://api.spacexdata.com/v4/launches/past"
 response = requests.get(url)
 response.json()
+
+
+data = pd.json_normalize(response.json())
+
+
+
+# Web Scraping - parse Falcon 9 Launch Records - using beatifulsoup
+
+# 1.wrangle data using API, 2.  3.deal with Null values - calc the mean, and replace nulls with mean
